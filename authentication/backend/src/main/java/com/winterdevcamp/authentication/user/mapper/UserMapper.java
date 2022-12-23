522df71b156323fbe9b7a5a6818c28683eb3e93e
devMuscle <hongsbien@naver.com>
2022-12-23 14:49:09 +0900

    feat: mapstruct 추가

---
+++ authentication/backend/src/main/java/com/winterdevcamp/authentication/user/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.winterdevcamp.authentication.user.mapper;
+
+import com.winterdevcamp.authentication.user.UserEntity;
+import com.winterdevcamp.authentication.user.dto.SignUpReqDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserEntity toEntity(SignUpReqDto signUpReqDto);
+}
