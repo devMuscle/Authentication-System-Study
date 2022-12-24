a03de755f7678e89605ee8b4a24b466718fa750a
devMuscle <hongsbien@naver.com>
2022-12-25 01:44:01 +0900

    chore: 사용하지 않는 라이브러리 import 제거

--- authentication/backend/src/main/java/com/winterdevcamp/authentication/authentication/AuthenticationController.java
+++ authentication/backend/src/main/java/com/winterdevcamp/authentication/authentication/AuthenticationController.java
@@ -2,16 +2,11 @@ package com.winterdevcamp.authentication.authentication;
 
 import com.winterdevcamp.authentication.dto.AuthenticationReqDto;
 import com.winterdevcamp.authentication.dto.TokenDto;
-import com.winterdevcamp.authentication.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.el.parser.Token;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,8 +17,7 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("")
-    public ResponseEntity<TokenDto> login(@RequestBody AuthenticationReqDto authenticationReqDto) {
-
+    public ResponseEntity<TokenDto> logIn(@RequestBody AuthenticationReqDto authenticationReqDto) {
         try {
             TokenDto tokenDto = authenticationService.authenticate(authenticationReqDto);
 
@@ -34,5 +28,4 @@ public class AuthenticationController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
-
 }
